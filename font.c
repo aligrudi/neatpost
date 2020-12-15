3fb3f39df51a9ecc59b7c9c9c84fadc25522164f
Ali Gholami Rudi <ali@rudi.ir>
2020-12-15 17:36:57 +0330

    font: prevent glyph name buffer overflow

--- font.c
+++ font.c
@@ -41,8 +41,8 @@ static int font_glyphput(struct font *fn, char *id, char *name, int type)
 		fn->gl = mextend(fn->gl, fn->gl_n, fn->gl_sz, sizeof(fn->gl[0]));
 	}
 	g = &fn->gl[fn->gl_n];
-	strcpy(g->id, id);
-	strcpy(g->name, name);
+	snprintf(g->id, sizeof(g->id), "%s", id);
+	snprintf(g->name, sizeof(g->name), "%s", name);
 	g->type = type;
 	g->font = fn;
 	dict_put(fn->gl_dict, g->id, fn->gl_n);
